afa0f67223856b8166a1c17d84537da0625ca523
Melih Gunal <gunalmel@med.umich.edu>
2017-05-23 16:14:48 -0400

    cache busting for javascript files and managing the html files with webpack is completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,72 @@
-var path = require('path');
-var webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in pluginsar path = 
+const path = require('path');
 
-var DIST_DIR = path.resolve(__dirname, 'dist');
-var SRC_DIR = path.resolve(__dirname, 'src');
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const SRC_DIR = path.resolve(__dirname, 'src');
 
-var config = {
+const config = {
+    //devtool: 'cheap-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, "dist/app"),
+        compress: true,
+        port: 9000
+    },
     entry: [SRC_DIR + '/app/index.js'],
     output: {
         path: DIST_DIR + '/app',
-        filename: 'bundle.js',
-        publicPath: '/app/'
+        filename: 'bundle.[hash].js',
+        publicPath: './'
     },
     module: {
-        loaders: [
+        rules:[
             {
-                test: /\.js?/,
-                include: SRC_DIR,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015', 'stage-2']
-                }
+                test: /\.(js|jsx)$/,
+                use: [
+                    { loader:'babel-loader'},
+                    { loader: 'eslint-loader'}
+                ],
+                exclude: /node_modules/
             },
             {
-                test: /\.js?/,
-                exclude: /node_modules/,
-                loader: ['babel-loader', 'eslint-loader']
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
             },
             {
                 test: /\.scss$/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader']
+                use:  [
+                    { loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    },
+                    { loader: 'sass-loader'}
+                ]
             }
         ]
-    }
+    },
+    plugins:[
+        //new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'My title',
+            showErrors: true,
+            xhtml: true,
+            inject: 'body', //true | 'head' | 'body' | false
+            //hash: true, //using the hash in file name in webpack config no need for this
+            minify: {},
+        })
+    ]
 };
 
 module.exports = config;
